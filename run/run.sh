d570a93e8baf6ef423eb7af3cc7808173bb832a3
강동현 <wel1come1234@login01>
2025-09-17 18:07:32 +0900

    kisti

--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J test_kdh
 #SBATCH -p batch
-#SBATCH -w cpu02
+#SBATCH -w cpu06
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1      # 물리 코어 수만큼 랭크
+#SBATCH --ntasks-per-node=64
 #SBATCH -o results/%x_%j.out
 #SBATCH -e results/%x_%j.err
 #SBATCH --comment xxx
@@ -11,4 +11,4 @@
 module purge 
 module load nvhpc/23.7
 
-mpirun -np 1 ./a.out ./PARA_INPUT.txt
+mpirun -np 64 ./a.out ./PARA_INPUT.txt
